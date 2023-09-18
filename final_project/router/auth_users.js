d5102343f7603fd3723eb77ca98331f1c896ae9e
Sheenasalin <138129275+Sheenasalin@users.noreply.github.com>
2023-09-18 17:00:33 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,21 +7,80 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
+}
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+  const email = req.params.email;
+  let user = users[email]
+  if (user) { //Check is user exists
+      let review = req.body.review;
+      let author = req.body.author;
+      let title=req.body.title
+      //if DOB the DOB has been changed, update the DOB 
+      if(review) {
+          books["review"] = review
+      }
+      if(author) {
+        books["author"] = author
+      }
+      if(title) {
+          books["title"] = title
+      }
+      books[title]=title;
+      res.send(`Books with the title  ${title} has been updated.`);
+  }
+  else{
+      res.send("Unable to find the book");
+  }
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
