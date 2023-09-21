d38d470c4c450b5426bdab3d0edebf8a9725391c
Sheenasalin <138129275+Sheenasalin@users.noreply.github.com>
2023-09-21 18:21:30 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,6 @@ public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
-
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4));
@@ -24,20 +23,55 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author=req.params.author;
-  res.send(books[author])
+    const author=req.params.author;
+   /* Object.values(books).forEach( book => {
+        if (book.author==author)
+        {
+            res.send(book) 
+        }
+    })*/
+    /*Object.key(books).forEach(isbn =>
+        { 
+            const book=books[isbn];
+            if(book.author==author)       
+            {
+                res.send(book) 
+            }
+            
+        })*/
+   const filteredbooks = Object.values(books).filter(book =>
+    {                 
+        /*if(book.author==author)       
+        {
+                    return true;
+        }
+         return false;*/
+         return book.author==author;
+     })
+     res.send(filteredbooks)
+     
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title=req.params.title;
-    res.send(books[title])
+const title=req.params.title;
+const filteredbooks = Object.values(books).filter(book =>
+{                 
+            /*if(book.author==author)       
+            {
+                        return true;
+            }
+             return false;*/
+             return book.title==title;
+         })
+         res.send(filteredbooks)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    const reviews=req.params.reviews;
-    res.send(books[reviews])
+    const isbn=req.params.isbn;
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
+
